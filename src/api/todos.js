20b475883202d7cc76aafde4e78f87de53bf07f3
emily40830 <emily40830@gmail.com>
2022-10-03 23:29:30 +0800

    feat: add todo crud function

---
+++ src/api/todos.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3004';
+
+const getTodos = () => {};
+
+const createTodo = () => {};
+
+const patchTodo = () => {};
+
+const deleteTodo = () => {};
