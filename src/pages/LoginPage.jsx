474404d995f0c9ecb8c683410898feb7b329e1ce
emily40830 <emily40830@gmail.com>
2022-11-02 22:09:34 +0800

    feat: add login funciton to login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,10 +8,22 @@ import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { login } from '../api/auth';
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const handleClick = async () => {
+    try {
+      const { authToken } = await login({
+        username,
+        password,
+      });
+      localStorage.setItem('authToken', authToken);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <AuthContainer>
       <div>
@@ -37,7 +49,7 @@ const LoginPage = () => {
           onChange={(passwordInputValue) => setPassword(passwordInputValue)}
         />
       </AuthInputContainer>
-      <AuthButton>登入</AuthButton>
+      <AuthButton onClick={handleClick}>登入</AuthButton>
       <Link to="/signup">
         <AuthLinkText>註冊</AuthLinkText>
       </Link>
